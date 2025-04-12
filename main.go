e8c6741cbf919bbff73e09acec25ea66a9c98a3c
Кот-кайфун Новый <Vladimir_225meil.ru@mail.ru>
2025-04-12 08:43:56 +0300

    Что-то

---
+++ main.go
@@ -0,0 +1,17 @@
+// Вызывает работу программы и импортированных пакетов и функций
+package main
+
+import "fmt"
+
+func main() {
+	arr := "helo435ohMygod./m,*79384)()(_+=`)"
+
+	for i := range arr {
+		if arr[i] >= '0' && arr[i] <= '9' {
+			fmt.Print(string(arr[i]))
+		}
+	}
+
+	fmt.Println()
+	fmt.Println("Hello, I am ToDo list")
+}
