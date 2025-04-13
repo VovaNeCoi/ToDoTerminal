e9bfa7a58c8c762973529619029735595e00cc8c
Кот-кайфун Новый <Vladimir_225meil.ru@mail.ru>
2025-04-13 08:10:26 +0300

    Добавил функцию создания файла,  обработку ошибок

--- Functionality/todoAdd.go
+++ Functionality/todoAdd.go
@@ -1,7 +1,45 @@
+// Функция добавления новых данных в файл. Функция получает номер задачи,
+// краткое название и комментарий с пояснением. Дальше можно накинуть проверку
+// существования таски
 package functionality
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
-func Add() {
-	fmt.Print("Hello, I am adding function")
+// Создаем структуру для хранения данных в определенном виде. Пока неясно будет ли вообще использоваться
+// Пока не использую
+type fileDataStruct struct {
+	taskNum   int
+	shortName string
+	comment   string
+}
+
+// Добавляем созданные данные в файл. Каждое новое добавление должно вызывать функцию Add
+func Add(num int, shName, comm string) {
+	dataForSerialize := &fileDataStruct{taskNum: num, shortName: shName, comment: comm}
+
+	// Сериализовываем данные
+	dataForFile, err := json.Marshal(dataForSerialize)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	// Создаю обычный бинарный файл
+	f, err := os.Create("OurToDoList.json")
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	// Нужно реализовать правильную обработку ошибоу через panic и вывод ошибки в виде перменной err
+	err = ioutil.WriteFile(f.Name(), dataForFile, 0644)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
 }
