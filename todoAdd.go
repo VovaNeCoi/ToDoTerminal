b90d37977789b9b0a8a95c0677179a85402ff2f1
Кот-кайфун Новый <Vladimir_225meil.ru@mail.ru>
2025-04-16 20:11:27 +0300

    Функция добавления (Add) Добавляет данные, но не проверяет на наличие дупликатов

--- todoAdd.go
+++ todoAdd.go
@@ -1,7 +1,7 @@
 // Функция добавления новых данных в файл. Функция получает номер задачи int,
 // краткое название string и комментарий с пояснением string. Дальше можно накинуть проверку
 // существования таски
-package functionality
+package main
 
 import (
 	"encoding/json"
@@ -12,25 +12,39 @@ import (
 // Создаем структуру для хранения данных в определенном виде.
 // Добавить пометки для полей
 type FileDataStruct struct {
-	TaskNum   int
-	ShortName string
-	Comment   string
+	TaskNum   int    `json:"Номер задачи"`
+	ShortName string `json:"Название"`
+	Comment   string `json:"Комментарий"`
 }
 
 // Добавляем созданные данные в файл. Каждое новое добавление должно вызывать функцию Add
 func Add(num int, shName, comm string) {
-	dataForSerialize := &FileDataStruct{TaskNum: num, ShortName: shName, Comment: comm}
+	// Название файла
+	fileName := "OurToDoList.json"
+
+	// Нужно читать данные с файла и к ним добавлять новые **Добавить обработку ошибки нулевого файла
+	tmp, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Ошибка чтения файла, ", err)
+	}
+
+	// Создается слайс с типом данных FileDataStruct
+	var dataForSerialize []FileDataStruct
+	json.Unmarshal(tmp, &dataForSerialize)
+
+	newData := &FileDataStruct{TaskNum: num, ShortName: shName, Comment: comm}
+	dataForSerialize = append(dataForSerialize, *newData)
+
+	fmt.Println(dataForSerialize)
 
 	// Сериализовываем данные. Они хранятся в типе байт
-	dataForFile, err := json.Marshal(dataForSerialize)
+	dataForFile, err := json.MarshalIndent(dataForSerialize, " ", " ")
 	if err != nil {
 		fmt.Println("В сериализации ошибка", err)
 		return
 	}
 
-	// Название файла
-	fileName := "OurToDoList.json"
-	// Создаю обычный json файл
+	// Создаю обычный json файл С проверкой существования файла
 	if _, err := os.Stat("OurToDoList.json"); os.IsNotExist(err) {
 		_, err := os.Create("OurToDoList.json")
 		if err != nil {
@@ -48,3 +62,4 @@ func Add(num int, shName, comm string) {
 }
 
 // Для дальнейшего масштабирования. Можно номер задачи задавать не вручную, а автоматически
+// Можно добавить шаги для задач, их нумерацию и дать возможность удалять их и добавлять
