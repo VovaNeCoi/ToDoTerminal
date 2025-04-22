ab8b9dba8d9d1e079f89770e7f78cd2b302e1e1c
Titov Igor <igortitov995@gmail.com>
2025-04-22 21:36:09 +0400

    New UI

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,36 @@ import (
 	functions "projMod/internal/todoServices"
 )
 
+func companion() {
+	var operator string
+	fmt.Println("Choose command (write number):")
+	fmt.Println("1. Output ToDo list")
+	fmt.Println("2. Add line")
+	fmt.Println("3. Delete line")
+	fmt.Println("4. Exit")
+	fmt.Scanln(&operator)
+	switch operator {
+	case "1":
+		functional.ReadFromFile()
+	case "2":
+		functional.AddTask()
+	case "3":
+		functional.DeleteTask()
+	case "4":
+		break
+	default:
+		fmt.Println("Unknown command")
+		return
+	}
+
 func main() {
+
+	fmt.Println("Hello, I am ToDo list")
+	fmt.Println("How I can help you?")
+
+	var TaskNum int
+	var ShortName, comm string
+	
 	var err error
 	fmt.Println("Hello, I am ToDo list")
 
