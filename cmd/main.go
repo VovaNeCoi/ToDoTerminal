0fc3febecbe54f9fd1fa2fadad3a1c9fd67bc319
VovaNeCoi <102732956+VovaNeCoi@users.noreply.github.com>
2025-04-26 20:55:29 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -19,14 +19,4 @@ func main() {
 
 	var err error
 	fmt.Println("Goodbye, I am ToDo list")
-
-	// err = functions.AddTask(2, "Покакать", "Нужно открыть дверь, зайти в туалет, снять штаны, сделать 'дела', вытереть жопу, надеть штаны")
-	// if err != nil {
-	// 	log.Printf("Ошибка добавления: %s", err)
-	// }
-
-	err = functions.DeleteTask(3)
-	if err != nil {
-		log.Printf("Ошибка удаления: %v", err)
-	}
 }
