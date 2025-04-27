3413fcb5329410a19e4a2e567ae048c658f99bb1
Кот-кайфун Новый <Vladimir_225meil.ru@mail.ru>
2025-04-27 22:05:09 +0300

    1

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,6 @@
 // Вызывает работу программы и импортированных пакетов и функций
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	var err error
-	fmt.Println("Hello, I am ToDo list", err)
 
 }
